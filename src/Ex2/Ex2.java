efc1e07b00bb2f6af6aa26cf2ae6b3bfa9804ae1
Andy <scorpion.and@ukr.net>
2016-11-17 16:50:37 +0200

    Виконано завданн №2

---
+++ src/Ex2/Ex2.java
@@ -0,0 +1,33 @@
+package Ex2;
+
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Rectangle square = createRec();
+    }
+
+    private static Rectangle createRec() {
+        Scanner scn = new Scanner(System.in);
+        Rectangle rec = new Rectangle();
+        int exit = 20;
+        do {
+            System.out.println("Вести данні - 1");
+            System.out.println("Обрахувати - 2");
+            System.out.println("Вихід - 0");
+            System.out.print("Ваш вибір >");
+            exit = scn.nextInt();
+            if (exit == 1) {
+                System.out.print("Введіть висоту >");
+                rec.height = scn.nextDouble();
+                System.out.print("Введіть ширину >");
+                rec.width = scn.nextDouble();
+            }
+            if (exit == 2) {
+               rec.getArea(rec.height, rec.width);
+            }
+        } while (exit > 0);
+        return rec;
+    }
+}
---
+++ src/Ex2/Rectangle.java
@@ -0,0 +1,12 @@
+package Ex2;
+
+
+public class Rectangle {
+    double width = 10.0;
+    double height = 10.0;
+
+    public double getArea(double a, double b) {
+        System.out.println(a*b);
+        return a*b;
+    }
+}
