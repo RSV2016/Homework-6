99c5c3c138cd3b68678b212372ff7dc6c1c6b6c6
Sergey <java.courses.2016@gmail.com>
2016-11-21 01:38:10 +0200

    Ex3

--- src/Ex3/Human.java
+++ src/Ex3/Human.java
@@ -1,14 +1,13 @@
 package Ex3;
 
-
 public class Human {
     String name;
     String secondName;
     int age;
 
-    public void printOut(String name, String secondName,int age){
-        System.out.println("І'мя >" + name);
-        System.out.println("Фамілія >" + secondName);
-        System.out.println("Вік >" + age);
+    public void printOut(){
+        System.out.println("І'мя >" + this.name);
+        System.out.println("Фамілія >" + this.secondName);
+        System.out.println("Вік >" + this.age);
     }
 }
